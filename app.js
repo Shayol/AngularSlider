1b7731ed7910d7e31ef61966f868802cb45a064e
Shayol <vorga88@gmail.com>
2015-05-15 13:27:35 +0300

    create service for eliminating code duplication

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 var sliderApp = angular.module('sliderApp', ['ngAnimate']);
 
-sliderApp.controller('SliderR', function($scope, $rootScope, $timeout) {
+sliderApp.factory('sliderService', function(){
+  return {
+    initialize: function($scope){
+
+      $scope.shuffle = function(){
+          for(var j, x, i = $scope.images.length; i; j = Math.floor(Math.random() * i), x = $scope.images[--i], $scope.images[i] = $scope.images[j], $scope.images[j] = x);
+          return $scope.images;
+      };
+
+      $scope.next = function() {
+          $scope.currentIndex < $scope.images.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
+        };
+
+      $scope.prev = function() {
+        $scope.currentIndex > 0 ? $scope.currentIndex-- : $scope.currentIndex = $scope.images.length - 1;
+      };
+      $scope.setCurrentSlideIndex = function (index) {
+          $scope.currentIndex = index;
+      };
+
+      $scope.isCurrentSlideIndex = function (index) {
+          return $scope.currentIndex === index;
+      };
+    }
+  }
+});
+
+sliderApp.controller('SliderR', function($scope, $rootScope, $timeout, sliderService) {
+  sliderService.initialize($scope);
+
   $scope.images = [{
   src: 'img/img1.png',
     id: 1
@@ -26,57 +55,34 @@ sliderApp.controller('SliderR', function($scope, $rootScope, $timeout) {
 
     //watch for changes of length of images collection and broadcast them to controller SliderL
 
-    $scope.$watchCollection('images', function(newVal, oldValue) {
-      if(newVal.length != oldValue.length){
-            $rootScope.$broadcast('addImage', newImage);
-          }
-        });
-
-    //make collection images random
-
-  $scope.shuffle = function(){
-      for(var j, x, i = $scope.images.length; i; j = Math.floor(Math.random() * i), x = $scope.images[--i], $scope.images[i] = $scope.images[j], $scope.images[j] = x);
-      return $scope.images;
-  };
-
+  $scope.$watchCollection('images', function(newVal, oldValue) {
+    if(newVal.length != oldValue.length){
+          $rootScope.$broadcast('addImage', newImage);
+        }
+      });
 
   $scope.currentIndex = 0; // Initially the index is at the first image
 
-    //slide navigation
+  // timer
 
-    $scope.next = function() {
-      $scope.currentIndex < $scope.images.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
-    };
+  var timer;
+  var sliderFunc = function() {
+    timer = $timeout(function() {
+      // reverse the order
+      $scope.next();
+      timer = $timeout(sliderFunc, 5000);
+    }, 5000);
+  };
 
-    $scope.prev = function() {
-      $scope.currentIndex > 0 ? $scope.currentIndex-- : $scope.currentIndex = $scope.images.length - 1;
-    };
-    $scope.setCurrentSlideIndex = function (index) {
-        $scope.currentIndex = index;
-    };
-
-    $scope.isCurrentSlideIndex = function (index) {
-        return $scope.currentIndex === index;
-    };
-
-    //timer
-
-    var timer;
-    var sliderFunc = function() {
-      timer = $timeout(function() {
-        $scope.next();
-        timer = $timeout(sliderFunc, 5000);
-      }, 5000);
-    };
-
-    sliderFunc();
-
-    $scope.$on('$destroy', function() {
-      $timeout.cancel(timer); // when the $scope is getting destroyed, cancel the timer
-    });
+  sliderFunc();
+  $scope.$on('$destroy', function() {
+    $timeout.cancel(timer);
+  });
 });
 
-sliderApp.controller('SliderL', function($scope, $timeout) {
+sliderApp.controller('SliderL', function($scope, $timeout, sliderService) {
+  sliderService.initialize($scope);
+
   $scope.images = [{
   src: 'img/img1.png',
     id: 1
@@ -96,45 +102,25 @@ sliderApp.controller('SliderL', function($scope, $timeout) {
 
   // listen for changes in controller SliderR
 
-  $scope.$on('addImage',function(event,data){
+  $scope.$on('addImage', function(event,data){
     $scope.images.push(data);
   });
 
-    $scope.shuffle = function(){
-      for(var j, x, i = $scope.images.length; i; j = Math.floor(Math.random() * i), x = $scope.images[--i], $scope.images[i] = $scope.images[j], $scope.images[j] = x);
-      return $scope.images;
-  };
   $scope.currentIndex = $scope.images.length - 1;
-
-    $scope.next = function() {
-      $scope.currentIndex < $scope.images.length - 1 ? $scope.currentIndex++ : $scope.currentIndex = 0;
-    };
-
-    $scope.prev = function() {
-      $scope.currentIndex > 0 ? $scope.currentIndex-- : $scope.currentIndex = $scope.images.length - 1;
-    };
-    $scope.setCurrentSlideIndex = function (index) {
-        $scope.currentIndex = index;
-    };
-
-    $scope.isCurrentSlideIndex = function (index) {
-        return $scope.currentIndex === index;
-    };
 
     // timer
 
-    var timer;
-    var sliderFunc = function() {
-      timer = $timeout(function() {
-        // reverse the order
-        $scope.prev();
-        timer = $timeout(sliderFunc, 5000);
-      }, 5000);
-    };
+  var timer;
+  var sliderFunc = function() {
+    timer = $timeout(function() {
+      // reverse the order
+      $scope.prev();
+      timer = $timeout(sliderFunc, 5000);
+    }, 5000);
+  };
 
-    sliderFunc();
-
-    $scope.$on('$destroy', function() {
-      $timeout.cancel(timer); // when the $scope is getting destroyed, cancel the timer
-    });
+  sliderFunc();
+  $scope.$on('$destroy', function() {
+    $timeout.cancel(timer);
+  });
 });
